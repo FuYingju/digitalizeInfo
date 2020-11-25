f9790668d0681d5b8673e290bff834804b4daef4
FuYingju <980084722@qq.com>
2020-11-25 09:52:50 +0800

    合作伙伴js

---
+++ src/api/common/partner.js
@@ -0,0 +1,45 @@
+import { request } from '@/utils/request'
+
+/**
+ * 查询品牌
+ */
+export function listBrandName() {
+  return request({
+    url: 'api/ruoyi/partnerNews/listBrandName',
+    // data: data,
+    method: 'post'
+  })
+}
+
+/**
+ * 查询全部合作伙伴动态
+ */
+export function selectAllHeziPartnerNews() {
+  return request({
+    url: 'api/ruoyi/partnerNews/list',
+    // data: data,
+    method: 'post'
+  })
+}
+
+/**
+ * 根据品牌查标题
+ */
+export function getTitleByBrandName(data) {
+  return request({
+    url: 'api/ruoyi/partnerNews/listByBrandName',
+    data: data,
+    method: 'post'
+  })
+}
+
+/**
+ * 根据标题ID查内容
+ */
+export function listById(data) {
+  return request({
+    url: 'api/ruoyi/partnerNews/listById',
+    data: data,
+    method: 'post'
+  })
+}
