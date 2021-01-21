1be52dab0c9e89611ba299e104bdd290219ffa01
FuYingju <980084722@qq.com>
2021-01-21 15:14:51 +0800

    1

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // API_ROOT:'"/api"'
-  API_ROOT:'"http://10.133.64.45/"'
+  API_ROOT:'"http://http://39.100.100.211/"'
 })
